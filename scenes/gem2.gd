fd8237bc96718505321823602a70dbd1e9ce4864
Hong Anh <honganh@HongnoMacBook-Pro.local>
2025-11-15 18:15:59 -0500

    changes

---
+++ scenes/gem2.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+  # Check if the event is a mouse button click
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			queue_free()
---
+++ scenes/gem5.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+  # Check if the event is a mouse button click
+	if event is InputEventMouseButton:
+	# If the left mouse button is pressed
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			queue_free()
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		queue_free()
